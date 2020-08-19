741b963560235721821f2fd87aba65ede024cbae
Matt Superdock <msuperdock@gmail.com>
2020-08-19 16:34:37 -0400

    migrate to niv for dependency management

--- shell.nix
+++ shell.nix
@@ -1,6 +1,17 @@
 let
-  pkgs = import <unstable> {};
+
+  sources = import ./nix/sources.nix;
+  packages = import sources.nixpkgs {};
+  derivation = packages.haskellPackages.callPackage ./default.nix {};
 
 in
-  (pkgs.haskellPackages.callPackage ./default.nix {}).env
+
+  derivation.env.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [
+      packages.cabal-install
+      packages.haskellPackages.Agda
+      (packages.haskellPackages.callPackage
+        /data/code/agda-unused/default.nix {})
+    ];
+  })
 
