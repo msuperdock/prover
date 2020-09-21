bc8ff8ddede75df7ef1856de493006da286d162f
Matt Superdock <msuperdock@gmail.com>
2020-09-21 12:29:09 -0400

    automate checking of all root files

--- shell.fish
+++ shell.fish
@@ -12,19 +12,34 @@ function with-dir
   cd $dir; return $st
 end
 
+function check-agda
+  argparse --name=check-agda 'a/all' -- $argv
+
+  if test $_flag_all
+    echo-space 'Checking Agda code:'
+  end
+
+  set modules (agda-roots --root /data/code/prover/src); or return $status
+
+  for module in $modules
+    echo-space "Checking $module:"
+    with-dir /data/code/prover/src agda \
+      --local-interfaces \
+      --no-libraries \
+      --include-path=. \
+      $module
+    or return $status
+  end
+
+  agda-unused
+end
+
 function build-agda
   argparse --name=build-agda 'a/all' -- $argv
 
-  if test $argv[1]
-    set file $argv[1]
-    set main '--no-main'
-  else
-    set file 'Main.agda'
-    set main ''
-  end
-
   if test $_flag_all
-    echo-space 'Building agda code:'
+    check-agda --all; or return $status
+    echo-space 'Building Agda code:'
   end
 
   with-dir /data/code/prover/src agda \
@@ -33,16 +48,15 @@ function build-agda
     --local-interfaces \
     --no-libraries \
     --include-path=. \
-    $main \
-    $file
+    Main.agda
 end
 
-function build-exe
+function build-hs
   argparse --name=build-exe 'a/all' -- $argv
 
   if test $_flag_all
     build-agda --all; or return $status
-    echo-space 'Building executable:'
+    echo-space 'Building Haskell code:'
   end
 
   with-dir /data/code/prover cabal new-build prover
@@ -53,7 +67,7 @@ function run
 
   if test $_flag_all
     build-agda --all; or return $status
-    echo-space 'Running haskell executable:'
+    echo-space 'Running Haskell executable:'
   end
 
   with-dir /data/code/prover cabal new-run
